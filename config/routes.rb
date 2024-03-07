f61db54c9c8a5f68ad85b3ff76356e16be230846
Shafaq-Juda <sjuda008@gmail.com>
2024-03-07 16:20:50 -0600

    created show page for user/:username

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "photos#index"
 
+  #get "/users/:id" => "users#show", as: :user
   devise_for :users
  
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
-
+ 
+  get "/:username" => "users#show", as: :user
 end
